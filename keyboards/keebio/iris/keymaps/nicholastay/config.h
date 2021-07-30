fa1f3528da6e1e8e4b703c502b45ad56625a11b6
Nicholas Tay <nick@windblume.net>
2021-07-30 17:56:01 +1000

    Reduce delay and add custom product meta

--- keyboards/keebio/iris/keymaps/nicholastay/config.h
+++ keyboards/keebio/iris/keymaps/nicholastay/config.h
@@ -1,3 +1,10 @@
 #pragma once
 
+#undef PRODUCT
+
+#define PRODUCT "Nick's Iris Keyboard"
+#undef MANUFACTURER
+#define MANUFACTURER "Nicholas Tay <nick@windblume.net>"
+
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define TAPPING_TERM 150
