2da64d5a1ae46609badbab512529cc6f91970e23
Nicholas Tay <nick@windblume.net>
2021-07-30 18:11:34 +1000

    Enable mousekeys for my config with some tuned params
    Needed to disable something - chose the console - for the mouse keys
    module to fit in the iris flash

--- keyboards/keebio/iris/keymaps/nicholastay/config.h
+++ keyboards/keebio/iris/keymaps/nicholastay/config.h
@@ -8,3 +8,9 @@
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 #define TAPPING_TERM 150
+
+// mousekey settings - 8 for 120hz monitor
+#define MOUSEKEY_INTERVAL 8
+#define MOUSEKEY_TIME_TO_MAX 120
+#define MOUSEKEY_DELAY 100
+#define MOUSEKEY_MAX_SPEED 7
